24e3699de4f544768b21ff3c7706713ff1f9c82a
Afolarinwa12 <Afolarinwa12@gmail.com>
2025-05-29 23:43:33 +0100

    task 5

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -28,11 +28,11 @@ return(
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const posts = await response.json()
+  const users = await response.json()
 
   return {
     props: {
-      posts
+      users
     }
   }
 }
